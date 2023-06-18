f312f9c208dd35cabbf474bb216bdbe54f2700d4
João Victor Guillen <guillenguilli@gmail.com>
2023-06-18 18:54:43 -0300

    adicionando navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <header>
+      <nav>
+          <router-link to="/veiculo">Veículo</router-link> |
+          <router-link to="/marca">Marca</router-link> |
+          <router-link to="/condutor">Condutor</router-link> |
+          <router-link to="/modelo">Modelo</router-link> |
+          <router-link to="/movimentacao">Movimentacao</router-link>
+      </nav>
+  </header>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script lang="ts">
+  export default {
+      name: 'NavBarComponent'
   }
-}
+</script>
+
+<style>
+  *{  font-family: Helvetica;
+  
+       margin: 0 auto;
+        padding: 0px;
+
+      box-sizing: border-box;
+  }
+    nav {
+      background-color: #222;
+      padding: 15px 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+  }
+
+  nav a
+  {
+      color: #FCBA03;
+      text-decoration: none;
+      margin: 12px;
+      transition: .5s;
+
+  }
+
+  nav a:hover{
+      color: #FFF;
+  }
 </style>
